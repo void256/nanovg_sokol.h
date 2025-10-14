29f39cd4f089922504a2299f10240a781068eac7
Luke Iannini <lukexi@me.com>
2025-10-14 15:12:54 -0700

    update sokol headers to match nanovg_sokol revision

--- src/sokol/sokol_log.h
+++ src/sokol/sokol_log.h
@@ -25,7 +25,7 @@
 
     Optionally define the following for verbose output:
 
-    SOKOL_DEBUG         - by default this is defined if _DEBUG is defined
+    SOKOL_DEBUG         - by default this is defined if NDEBUG is not defined
 
 
     OVERVIEW
@@ -326,15 +326,6 @@ SOKOL_API_IMPL void slog_func(const char* tag, uint32_t log_level, uint32_t log_
         __android_log_write(prio, "SOKOL", line_buf);
     #elif defined(_SLOG_EMSCRIPTEN)
         slog_js_log(log_level, line_buf);
-    #elif defined(_SLOG_LINUX) || defined(_SLOG_APPLE)
-        int prio;
-        switch (log_level) {
-            case 0: prio = LOG_CRIT; break;
-            case 1: prio = LOG_ERR; break;
-            case 2: prio = LOG_WARNING; break;
-            default: prio = LOG_INFO; break;
-        }
-        syslog(prio, "%s", line_buf);
     #endif
     if (0 == log_level) {
         abort();
